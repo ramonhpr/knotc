accc413d29b54356a957e58b813a675fd7eabe17
Ramon Ribeiro <rhpr@cesar.org.br>
2020-06-22 23:32:02 -0300

    Fix zephyr listener to generate two or more things

--- pkg/compiler/zephyr.go
+++ pkg/compiler/zephyr.go
@@ -25,7 +25,7 @@ func NewZephyrCompiler(outPath string) *ZephyrCompiler {
 }
 
 // Compile the input
-func (zc *ZephyrCompiler) Compile(input string) string {
+func (zc *ZephyrCompiler) Compile(input string) {
 	zc.Walk(input, &zc.listener)
 	tmp := template.New("compiler")
 	funcs := template.FuncMap{
@@ -33,54 +33,55 @@ func (zc *ZephyrCompiler) Compile(input string) string {
 		"Lower": strings.ToLower,
 	}
 	tmp.Funcs(funcs)
-	sourceCode := new(bytes.Buffer)
-	template.Must(tmp.Parse(tmplt.ZephyrSourceCode)).Execute(sourceCode, zc.listener)
-
-	x := struct {
-		Name string
-		Len int
-	}{Name: zc.listener.Name, Len: len(zc.listener.Sensors)}
-	prjConf := new(bytes.Buffer)
-	template.Must(tmp.Parse(tmplt.ZephyrPrjConf)).Execute(prjConf, x)
-
-	setupConf := new(bytes.Buffer)
-	template.Must(tmp.Parse(tmplt.ZephyrSetupConf)).Execute(setupConf, zc.listener)
-
-	cmake := new(bytes.Buffer)
-	template.Must(tmp.Parse(tmplt.ZephyrCMakeLists)).Execute(cmake, zc.listener)
-
-	err := zc.createTreeStructureFolder()
-	if err != nil {
-		panic(err)
+	
+	for _, thing := range zc.listener.Things {
+		sourceCode := new(bytes.Buffer)
+		template.Must(tmp.Parse(tmplt.ZephyrSourceCode)).Execute(sourceCode, thing)
+		
+		x := struct {
+			Name string
+			Len int
+		}{Name: thing.Name, Len: len(thing.Sensors)}
+		prjConf := new(bytes.Buffer)
+		template.Must(tmp.Parse(tmplt.ZephyrPrjConf)).Execute(prjConf, x)
+	
+		setupConf := new(bytes.Buffer)
+		template.Must(tmp.Parse(tmplt.ZephyrSetupConf)).Execute(setupConf, thing)
+	
+		cmake := new(bytes.Buffer)
+		template.Must(tmp.Parse(tmplt.ZephyrCMakeLists)).Execute(cmake, thing)
+	
+		err := zc.createTreeStructureFolder(thing.Name)
+		if err != nil {
+			panic(err)
+		}
+		err = zc.outputTopSrcDir(thing.Name, "proj.conf", prjConf)
+		if err != nil {
+			panic(err)
+		}
+		err = zc.outputTopSrcDir(thing.Name, "setup.conf", setupConf)
+		if err != nil {
+			panic(err)
+		}
+		err = zc.outputTopSrcDir(thing.Name, "CMakeLists.txt", cmake)
+		if err != nil {
+			panic(err)
+		}
+		err = zc.outputSrc(thing.Name, sourceCode)
+		if err != nil {
+			panic(err)
+		}
 	}
-	err = zc.outputTopSrcDir("proj.conf", prjConf)
-	if err != nil {
-		panic(err)
-	}
-	err = zc.outputTopSrcDir("setup.conf", setupConf)
-	if err != nil {
-		panic(err)
-	}
-	err = zc.outputTopSrcDir("CMakeLists.txt", cmake)
-	if err != nil {
-		panic(err)
-	}
-	err = zc.outputSrc(sourceCode)
-	if err != nil {
-		panic(err)
-	}
-
-	return sourceCode.String()
 }
 
-func (zc *ZephyrCompiler) createTreeStructureFolder() error {
-	return os.MkdirAll(filepath.Join(zc.outPath, "zephyr", zc.listener.Name, "src"), os.ModePerm)
+func (zc *ZephyrCompiler) createTreeStructureFolder(folderName string) error {
+	return os.MkdirAll(filepath.Join(zc.outPath, "zephyr", folderName, "src"), os.ModePerm)
 }
 
-func (zc *ZephyrCompiler) outputSrc(buf *bytes.Buffer) error {
-	return ioutil.WriteFile(filepath.Join(filepath.Join(zc.outPath, "zephyr", zc.listener.Name, "src", zc.listener.Name + ".c")), buf.Bytes(), 0644)
+func (zc *ZephyrCompiler) outputSrc(folderName string, buf *bytes.Buffer) error {
+	return ioutil.WriteFile(filepath.Join(filepath.Join(zc.outPath, "zephyr", folderName, "src", folderName + ".c")), buf.Bytes(), 0644)
 }
 
-func (zc *ZephyrCompiler) outputTopSrcDir(filename string, buf *bytes.Buffer) error {
-	return ioutil.WriteFile(filepath.Join(filepath.Join(zc.outPath, "zephyr", zc.listener.Name, filename)), buf.Bytes(), 0644)
+func (zc *ZephyrCompiler) outputTopSrcDir(folderName string, filename string, buf *bytes.Buffer) error {
+	return ioutil.WriteFile(filepath.Join(filepath.Join(zc.outPath, "zephyr", folderName, filename)), buf.Bytes(), 0644)
 }
